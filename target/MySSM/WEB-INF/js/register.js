bd01a617581cd63ce4d0a63b1b46f747e1efd379
zyd123-cmd <2621185486@qq.com>
2025-01-02 23:07:05 +0800

    1

---
+++ target/MySSM/WEB-INF/js/register.js
@@ -0,0 +1,43 @@
+// register.js
+$(document).ready(function() {
+    $('#registrationForm').submit(function(event) {
+        event.preventDefault(); // 阻止表单默认提交行为
+
+        const username = $('#username').val();
+        const password = $('#password').val();
+
+        // 准备要发送的数据
+        const data = {
+            "username": username,
+            "password": password
+        };
+
+        // 发起AJAX POST请求
+        $.ajax({
+            url: contextPath + "/user/register",
+            type: 'POST',
+            contentType: 'application/json', // 设置请求头内容类型为JSON
+            data: JSON.stringify(data), // 将JavaScript对象转换为JSON字符串
+            success: function(response) {
+                // 请求成功时的处理
+                if (response.success) {
+                    let countdown = 3;
+                    const intervalId = setInterval(function() {
+                        $('#responseMessage').text(`用户注册成功。将在${countdown}秒后跳转到登录页面`).css('color', 'green');
+                        countdown--;
+                        if (countdown < 0) {
+                            clearInterval(intervalId);
+                            location.href = contextPath + "/user/loginpage";
+                        }
+                    }, 1000);
+                } else {
+                    $('#responseMessage').text(response.message).css('color', 'red');
+                }
+            },
+            error: function(xhr, status, error) {
+                // 请求失败时的处理
+                $('#responseMessage').text('注册失败: ' + xhr.responseText).css('color', 'red');
+            }
+        });
+    });
+});
---
+++ target/MySSM/WEB-INF/jsp/success.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 十一月的肖邦
+  Date: 2024/12/30
+  Time: 9:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+  <h2>测试成功</h2>
+</head>
+<body>
+
+</body>
+</html>
