bd1ec7f323914332665817e7d64874411f2ac288
zyd123-cmd <2621185486@qq.com>
2025-01-02 11:04:10 +0800

    Delete src directory

--- src/main/java/com/cnlbc/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cnlbc.repository;
-
-import com.cnlbc.pojo.User;
-
-import java.util.List;
-
-public interface UserRepository {
-    public List<User> findAllUser();
-}
--- src/main/java/com/cnlbc/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.cnlbc.service;
-
-import com.cnlbc.pojo.User;
-
-import java.util.List;
-
-public interface UserService {
-    public List<User> findAllUser();
-}
